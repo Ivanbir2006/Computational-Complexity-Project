f52816eec6edbcb47a5f54ecd9964f5e46d13941
IvanBir2006 <vanya.biryukov.0609@mail.ru>
2026-05-18 20:57:13 +0300

    Description

--- main.cpp
+++ main.cpp
@@ -13,4 +13,7 @@ int main() {
 
   Graph g({{0, 3, 1}, {1, 3, 2}, {2, 3, 3}, {0, 3, 1}}, {0, 1, 2}, 4);
   std::cout << "Minimum multicut ->" << " " << g.GetMinimumMultiCut() << "\n";
+
+  Graph g2({{0,1,1},{1,2,1},{2,0,1}}, {0, 1, 2}, 3);
+  std::cout << "Minimum multicut ->" << " " << g2.GetMinimumMultiCut() << "\n";
 }
